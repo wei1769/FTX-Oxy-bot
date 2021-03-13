723fac6cca7950be814b5556466ac92a96d2a68a
Wei <williamlin910403@gmail.com>
2021-03-14 02:08:14 +0800

    Small changes

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
+#run 'pip3 install ftx' first 
 import ftx 
 import sys 
 apikey = ''  #api key
 apisecret = ''  #api secret
-limitPrice = #Price to bit 
+limitPrice = #Price to bid
 size =      #Size to bid 
 client = ftx.FtxClient(api_key = apikey, api_secret = apisecret)
 sys.tracebacklimit = 0
